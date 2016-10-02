88c0132db73010e100897b637c64e7ef8e8bb8d3
dashingshakeel <shkeelrauf@gmail.com>
2016-10-02 17:37:04 +0500

    Pagination added

--- lib/app.rb
+++ lib/app.rb
@@ -12,7 +12,15 @@ class App < Sinatra::Base
   end
 
   get "/issues" do
-    @issues = Issue.all
+    total_issues=10
+    current_page= params[:page].nil? ? 1 :params[:page].to_i
+    @issues = Issue.limit(total_issues).skip((current_page-1) * total_issues).all
+    last_page= Issue.count / total_issues
+    last_page = last_page.succ if Issue.count % total_issues
+    @pagination =OpenStruct.new({
+      last_page: last_page,
+      current_page: current_page  - 1 
+      })
     haml :"issues/index"
   end
 
@@ -31,11 +39,11 @@ class App < Sinatra::Base
     @issue= Issue.find params[:id]
     haml :"issues/edit"
   end
+
   put "/issues/:id" do
     @issue= Issue.find params[:id]
     if @issue.update_attributes params[:issue]
       redirect "/"
     end
-    
   end
 end
